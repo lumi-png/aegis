e5f6e3e59403154d8a6ddc6b2fd293e4fe87b00e
lumi-png <miguelbuccat@proton.me>
2026-04-03 08:50:58 +0800

    chore: scaffold user controller

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,17 @@
+const getUsers = () => {
+
+}
+
+const getUser = () => {
+
+}
+
+const createUser = () => {
+
+}
+
+const authUser = () => {
+  
+}
+
+export default { getUsers, getUser, createUser, authUser };
