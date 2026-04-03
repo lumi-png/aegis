15ecde6a1d8e5e87b18fe1a1cf1c06abd5dc623c
lumi-png <miguelbuccat@proton.me>
2026-04-03 08:05:27 +0800

    fix: change spacing to 2 on errorHandler.middleware.ts

--- server/src/middleware/errorHandler.middleware.ts
+++ server/src/middleware/errorHandler.middleware.ts
@@ -9,39 +9,39 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-    if (err instanceof ZodError) {
-        const apiError = new ApiError('Validation error', 400, err);
-        return res.status(apiError.statusCode).json({
-            success: false,
-            data: {
-                message: apiError.message,
-                formErrors: mapZodErrors(err)
-            }
-        });
-    }
+  if (err instanceof ZodError) {
+    const apiError = new ApiError('Validation error', 400, err);
+    return res.status(apiError.statusCode).json({
+      success: false,
+      data: {
+        message: apiError.message,
+        formErrors: mapZodErrors(err)
+      }
+    });
+  }
 
-    if (err instanceof ApiError) {
-        if(err.zodError) {
-            return res.status(err.statusCode).json({
-            success: false,
-            data: {
-                message: err.message,
-                formErrors: mapZodErrors(err.zodError)
-            }
-        });
-        }
-        return res.status(err.statusCode).json({
-            success: false,
-            data: {
-                message: err.message
-            }
-        });
-    }
-
-    return res.status(500).json({
+  if (err instanceof ApiError) {
+    if (err.zodError) {
+      return res.status(err.statusCode).json({
         success: false,
         data: {
-            message: "Internal Server Error"
+          message: err.message,
+          formErrors: mapZodErrors(err.zodError)
         }
+      });
+    }
+    return res.status(err.statusCode).json({
+      success: false,
+      data: {
+        message: err.message
+      }
     });
+  }
+
+  return res.status(500).json({
+    success: false,
+    data: {
+      message: "Internal Server Error"
+    }
+  });
 }
